30af67a13e7f722ad7c79f845bb857681c7603db
Uladzislau Rezki <urezki@gmail.com>
2009-08-17 23:51:28 +0300

    @MAINT:     1) Added print usage function;     2) The server can go to the background mode on demand;     3) Create pid file while starting, and remove it while stopping.
    Signed-off-by: Uladzislau Rezki <urezki@gmail.com>

--- src/sftpd.c
+++ src/sftpd.c
@@ -22,6 +22,7 @@
 #include <cmds.h>
 #include <mem.h>
 
+#define PID_FILE "/var/run/sftpd.pid"
 #define DEV_NULL "/dev/null"
 
 /*
@@ -32,130 +33,160 @@ static TAILQ_HEAD(cli_list, connection) conn_list;
 /* global variable */
 struct ftpd *srv = NULL;
 
-static ftpd *init_sftpd(void);
-/* static void init_daemon(void); */
-static int process_timeouted_sockets(int);
-static void process_commands(fd_set *, int *);
-static void process_transfers(fd_set *, fd_set *, int *);
-static int process_clients_marked_as_destroy(void);
-
-static connection *add_connection(int);
-static void destroy_connection(connection *);
-static void destroy_transport(connection *);
-static int clear_bad_fd();
-
-static inline void download_file(struct connection *);
-static inline void upload_file(struct connection *);
-
 static void
-accept_client(int socket, fd_set *r_fd, int *n_ready)
+print_usage(char **argv)
 {
-	struct sockaddr_in r_addr;
-	connection *conn = NULL;
-	socklen_t cli_len;
-	int conn_fd;
+	fprintf(stderr, "\n"
+			"Usage: %s [OPTION]\n"
+			"    -d    run server as daemon (background mode)\n"
+			"    -h    show this help message\n"
+			, argv[0]);
 
-	/*
-	 * We have to process situation when clients
-	 * send RST packet, but we are waiting for a connection
-	 * from other side ...
-	 */
-	if (FD_ISSET(socket, r_fd) && *n_ready > 0) {
-		cli_len = sizeof(r_addr);
-		conn_fd = accept(socket, (struct sockaddr *)&r_addr, &cli_len);
-		if (conn_fd != -1) {
-			activate_nodelay(conn_fd);
-			conn = add_connection(conn_fd);
-			if (conn != NULL)
-				send_cmd(conn->sock_fd, 220, "I'm ready");
-		} else {
-			PRINT_DEBUG("accept() error: %s\n", strerror(errno));
-		}
-
-		(*n_ready)--;
-	}
-}
-
-static void
-check_received_signals(void)
-{
-	if (signal_is_pending(SIGTERM) || signal_is_pending(SIGHUP)) {
-		struct connection *c;
-
-		TAILQ_FOREACH(c, &conn_list, entries) {
-			destroy_connection(c);
-		}
-
-		close(srv->srv_socket);
-		free(srv);
-		exit(1);				/* finish up */
-	}
+	exit(1);
 }
 
 static int
-wait_for_events(ftpd *sftpd, fd_set *r_fd, fd_set *w_fd, int sec)
+unlink_pid_file(const char *full_path)
 {
-	struct timeval time;
-	int n_ready;
+	int ret;
 
-	*r_fd = sftpd->read_ready;
-	*w_fd = sftpd->write_ready;
+	ret = unlink(full_path);
+	if (ret < 0)
+		PRINT_DEBUG("unlink error: %s\n", strerror(errno));
 
-	time.tv_sec = sec;
-	time.tv_usec = 0;
-
-	n_ready = select(FD_SETSIZE, r_fd, w_fd, NULL, &time);
-	if (n_ready < 0) {
-		if (errno == EBADF) {
-			PRINT_DEBUG("'select()' failed with errno %d: %s\n",
-						  errno, strerror(errno));
-			/* 
-			 * We mark bad descriptors, and below
-			 * will clean it up.
-			 */
-			clear_bad_fd();
-		}
-
-		sleep(1);
-	}
-
-	return n_ready;
+	return ret;
 }
 
-int main(int argc, char **argv)
+static int
+create_pid_file(const char *full_path)
 {
-	fd_set r_fd;
-	fd_set w_fd;
-	int n_ready;
+	FILE *pidfile;
+	int pidfd;
+	int pid;
+	int ret;
 
-	/* srv is global */
-	srv = init_sftpd();
+	pidfd = open(full_path, O_EXCL|O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	if (pidfd != -1) {
+		pidfile = fdopen(pidfd, "w");
+		if (pidfile) {
+			fchmod(pidfd, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+			fprintf(pidfile, "%d\n", getpid());
+			fclose(pidfile);
+		} else {
+			close(pidfd);
+			PRINT_DEBUG("fdopen error: %s\n", strerror(errno));
+			pidfd = -1;
+		}
+	} else {
+		if (errno == EEXIST) {
+			pidfile = fopen(full_path, "r+");
+			if (pidfile) {
+				(void) fscanf(pidfile, "%d", &pid);
+				(void) fclose(pidfile);
 
-	while (1) {
-		/* wait for any events, max is 1 min. */
-		n_ready = wait_for_events(srv, &r_fd, &w_fd, 60);
+				ret = kill(pid, 0);
+				if (ret < 0 && errno == ESRCH) {
+					PRINT_DEBUG("pid file %s exists, but the process is not alive",
+								full_path);
 
-		/* check signals */
-		check_received_signals();
+					ret = unlink_pid_file(full_path);
+					if (ret == 0)
+						return create_pid_file(full_path);
+				}
+			}
+		}
 
-		/* deal with exist transfers */
-		process_transfers(&r_fd, &w_fd, &n_ready);
-
-		/* process FTP's commands */
-		process_commands(&r_fd, &n_ready);
-
-		/*
-		 * We allow 10 minutes to users, for to do
-		 * something, after that clients are dropped.
-		 */
-		process_timeouted_sockets(10);
-		process_clients_marked_as_destroy();
-
-		/* accept new clients, if they are */
-		accept_client(srv->srv_socket, &r_fd, &n_ready);
+		PRINT_DEBUG("open %s error: %s\n", full_path, strerror(errno));
 	}
 
-	return 0;
+	return pidfd;
+}
+
+static void
+init_daemon(void)
+{
+	int i = 0;
+
+	FUNC_ENTRY();
+
+	if (fork() != 0)                /* quit first parent */
+		exit(0);
+	if (setsid() < 0)
+		perror("setsid");
+
+	if (fork() != 0)                /* quit second parent */
+		exit(0);
+
+	chdir("/");
+	umask(0);
+
+	for (i = 0; i < 64; i++)
+		close(i);
+
+	i = open(DEV_NULL, O_RDWR); /* stdin */
+	dup(i);						/* stdout */
+	dup(i);						/* stderr */
+
+	i = create_pid_file(PID_FILE);
+	if (i < 0)
+		exit(-1);
+
+	FUNC_EXIT_VOID();
+}
+
+static ftpd *
+init_sftpd(int argc, char **argv)
+{
+	ftpd *sftpd = NULL;
+	int fork_flag = 0;
+	int ch;
+
+	FUNC_ENTRY();
+
+	while ((ch = getopt(argc, argv, "dh")) != -1) {
+		switch (ch) {
+		case 'd':
+			fork_flag = 1;
+			break;
+
+		case 'h':
+			print_usage(argv);
+			break;
+
+		default:
+			;
+		}
+	}
+
+	if (fork_flag)
+		init_daemon();
+
+	signal_handle(SIGHUP);
+	signal_handle(SIGTERM);
+	signal_ignore(SIGPIPE);
+
+	/* allocate memory for our new ftp server */
+	sftpd = (ftpd *) calloc(1, sizeof(ftpd));
+	if (sftpd == NULL)
+		FATAL_ERROR("error allocating memory\n");
+
+	/* init some stuff */
+	sftpd->client_count = 0;
+	sftpd->srv_socket = -1;
+
+	sftpd->srv_socket = start_tcp_listen(21, 4, 30);
+	if (sftpd->srv_socket == -1)
+		FATAL_ERROR("error starting tcp listen\n");
+
+	FD_ZERO(&(sftpd->read_ready));
+	FD_ZERO(&(sftpd->write_ready));
+	FD_SET(sftpd->srv_socket, &(sftpd->read_ready));
+
+	/* initialize list */
+	TAILQ_INIT(&conn_list);
+
+	FUNC_EXIT_PTR(sftpd);
+	return sftpd;
 }
 
 static connection *
@@ -196,29 +227,6 @@ add_connection(int s)
 }
 
 static void
-destroy_connection(connection *conn)
-{
-	struct connection *c;
-
-	FUNC_ENTRY();
-
-	TAILQ_FOREACH(c, &conn_list, entries) {
-		if (c == conn) {
-			FD_CLR(conn->sock_fd, &(srv->read_ready));
-			close_socket(conn->sock_fd);
-			srv->client_count--;
-			TAILQ_REMOVE(&conn_list, c, entries);
-			destroy_transport(conn);
-			free(conn->transport);
-			free(conn);
-			break;	  /* !!! */
-		}
-	}
-
-	FUNC_EXIT_VOID();
-}
-
-static void
 destroy_transport(connection *conn)
 {
 	struct transport *t;
@@ -251,67 +259,136 @@ destroy_transport(connection *conn)
 	FUNC_EXIT_VOID();
 }
 
-/* static void */
-/* init_daemon(void) */
-/* { */
-/* 	int i = 0; */
-
-/* 	FUNC_ENTRY(); */
-	
-/* 	if (fork() != 0)                /\* quit first parent *\/ */
-/* 		exit(0); */
-/* 	if (setsid() < 0) */
-/* 		perror("setsid"); */
-
-/* 	if (fork() != 0)                /\* quit second parent *\/ */
-/* 		exit(0); */
-	
-/* 	chdir("/"); */
-/* 	umask(0); */
-
-/* 	for (i = 0; i < 64; i++) */
-/* 		close(i); */
-
-/* 	i = open(DEV_NULL, O_RDWR); /\* stdin *\/ */
-/* 	dup(i);						/\* stdout *\/ */
-/* 	dup(i);						/\* stderr *\/ */
-
-/* 	FUNC_EXIT_VOID(); */
-/* } */
-
-static ftpd *
-init_sftpd(void)
+static void
+destroy_connection(connection *conn)
 {
-	ftpd *sftpd = NULL;
-	
+	struct connection *c;
+
 	FUNC_ENTRY();
 
-	signal_handle(SIGHUP);
-	signal_handle(SIGTERM);
-	signal_ignore(SIGPIPE);
+	TAILQ_FOREACH(c, &conn_list, entries) {
+		if (c == conn) {
+			FD_CLR(conn->sock_fd, &(srv->read_ready));
+			close_socket(conn->sock_fd);
+			srv->client_count--;
+			TAILQ_REMOVE(&conn_list, c, entries);
+			destroy_transport(conn);
+			free(conn->transport);
+			free(conn);
+			break;	  /* !!! */
+		}
+	}
 
-	/* allocate memory for our new ftp server */
-	sftpd = (ftpd *) calloc(1, sizeof(ftpd));
-	if (sftpd == NULL)
-		FATAL_ERROR("error allocating memory\n");
-	
-	/* init some stuff */
-	sftpd->client_count = 0;
-	sftpd->srv_socket = -1;
+	FUNC_EXIT_VOID();
+}
 
-	sftpd->srv_socket = start_tcp_listen(21, 4, 30);
-	if (sftpd->srv_socket == -1)
-		FATAL_ERROR("error starting tcp listen\n");
+static void
+accept_client(int socket, fd_set *r_fd, int *n_ready)
+{
+	struct sockaddr_in r_addr;
+	connection *conn = NULL;
+	socklen_t cli_len;
+	int conn_fd;
 
-	FD_ZERO(&(sftpd->read_ready));
-	FD_ZERO(&(sftpd->write_ready));
-	FD_SET(sftpd->srv_socket, &(sftpd->read_ready));
+	/*
+	 * We have to process situation when clients
+	 * send RST packet, but we are waiting for a connection
+	 * from other side ...
+	 */
+	if (FD_ISSET(socket, r_fd) && *n_ready > 0) {
+		cli_len = sizeof(r_addr);
+		conn_fd = accept(socket, (struct sockaddr *)&r_addr, &cli_len);
+		if (conn_fd != -1) {
+			activate_nodelay(conn_fd);
+			conn = add_connection(conn_fd);
+			if (conn != NULL)
+				send_cmd(conn->sock_fd, 220, "I'm ready");
+		} else {
+			PRINT_DEBUG("accept() error: %s\n", strerror(errno));
+		}
 
-	/* initialize list */
-	TAILQ_INIT(&conn_list);
+		(*n_ready)--;
+	}
+}
 
-	FUNC_EXIT_PTR(sftpd);
-	return sftpd;
+static void
+check_received_signals(void)
+{
+	if (signal_is_pending(SIGTERM) || signal_is_pending(SIGHUP)) {
+		struct connection *c;
+
+		TAILQ_FOREACH(c, &conn_list, entries) {
+			destroy_connection(c);
+		}
+
+		unlink_pid_file(PID_FILE);
+		close(srv->srv_socket);
+		free(srv);
+		exit(1);				/* finish up */
+	}
+}
+
+static int
+clear_bad_fd(void)
+{
+	struct connection *c;
+	struct transport *t;
+
+	int processed = 0;
+	int ret;
+
+	FUNC_ENTRY();
+
+	TAILQ_FOREACH(c, &conn_list, entries) {
+		t = c->transport;
+
+		ret = check_socket(c->sock_fd);
+		if (ret == -1) {
+			SET_FLAG(c->c_flags, C_KILL);
+			processed++;
+		} else {
+			if (!QUERY_FLAG(t->t_flags, T_FREE)) {
+				ret = check_socket(t->socket);
+				if (ret == -1) {
+					SET_FLAG(t->t_flags, T_KILL);
+					processed++;
+				}
+			}
+		}
+	}
+
+	FUNC_EXIT_INT(processed);
+	return processed;
+}
+
+static int
+wait_for_events(ftpd *sftpd, fd_set *r_fd, fd_set *w_fd, int sec)
+{
+	struct timeval time;
+	int n_ready;
+
+	*r_fd = sftpd->read_ready;
+	*w_fd = sftpd->write_ready;
+
+	time.tv_sec = sec;
+	time.tv_usec = 0;
+
+	n_ready = select(FD_SETSIZE, r_fd, w_fd, NULL, &time);
+	if (n_ready < 0) {
+		if (errno == EBADF) {
+			PRINT_DEBUG("'select()' failed with errno %d: %s\n",
+						  errno, strerror(errno));
+
+			/*
+			 * It marks connections with bad descriptors.
+			 */
+			clear_bad_fd();
+		}
+
+		sleep(1);
+	}
+
+	return n_ready;
 }
 
 static int
@@ -419,6 +496,55 @@ restart:
 }
 
 static void
+download_file(struct connection *c)
+{
+	struct transport *t;
+	int n;
+
+	FUNC_ENTRY();
+
+	t = c->transport;
+	n = sendfile(t->socket, t->local_fd, &t->offset, t->st.st_blksize);
+	if (n > 0)
+		goto again;
+
+	send_cmd(c->sock_fd, 226, "Transfer complete.");
+	destroy_transport(c);
+again:
+	FUNC_EXIT_VOID();
+}
+
+static void
+upload_file(struct connection *c)
+{
+	struct transport *t;
+	char buf[65536];
+	int avail;
+	int size;
+
+	FUNC_ENTRY();
+
+	t = c->transport;
+	ioctl(t->socket, FIONREAD, &avail);
+	avail = avail > 65536 ? 65536:avail < 0 ? 0:avail;
+
+	size = recv_data(t->socket, buf, avail, 0);
+	if (size > 0) {
+		int n = write(t->local_fd, buf, size);
+		if (n == size)
+			goto again;
+		else if (n < 0) {
+			/* ??? */
+		}
+	}
+
+	send_cmd(c->sock_fd, 226, "Transfer complete.");
+	destroy_transport(c);
+again:
+	FUNC_EXIT_VOID();
+}
+
+static void
 process_transfers(fd_set *r_fd, fd_set *w_fd, int *n_ready)
 {
 	struct connection *c;
@@ -459,84 +585,38 @@ process_transfers(fd_set *r_fd, fd_set *w_fd, int *n_ready)
 	FUNC_EXIT_VOID();
 }
 
-static int
-clear_bad_fd(void)
+int main(int argc, char **argv)
 {
-	struct connection *c;
-	struct transport *t;
+	fd_set r_fd;
+	fd_set w_fd;
+	int n_ready;
 
-	int processed = 0;
-	int ret;
+	/* srv is global */
+	srv = init_sftpd(argc, argv);
 
-	FUNC_ENTRY();
+	while (1) {
+		/* wait for any events, max is 1 min. */
+		n_ready = wait_for_events(srv, &r_fd, &w_fd, 60);
 
-	TAILQ_FOREACH(c, &conn_list, entries) {
-		t = c->transport;
+		/* check signals */
+		check_received_signals();
 
-		ret = check_socket(c->sock_fd);
-		if (ret == -1) {
-			SET_FLAG(c->c_flags, C_KILL);
-			processed++;
-		} else {
-			if (!QUERY_FLAG(t->t_flags, T_FREE)) {
-				ret = check_socket(t->socket);
-				if (ret == -1) {
-					SET_FLAG(t->t_flags, T_KILL);
-					processed++;
-				}
-			}
-		}
+		/* deal with exist transfers */
+		process_transfers(&r_fd, &w_fd, &n_ready);
+
+		/* process FTP's commands */
+		process_commands(&r_fd, &n_ready);
+
+		/*
+		 * We allow 10 minutes to users, for to do
+		 * something, after that clients are dropped.
+		 */
+		process_timeouted_sockets(10);
+		process_clients_marked_as_destroy();
+
+		/* accept new clients, if they are */
+		accept_client(srv->srv_socket, &r_fd, &n_ready);
 	}
 
-	FUNC_EXIT_INT(processed);
-	return processed;
-}
-
-static inline void
-download_file(struct connection *c)
-{
-	struct transport *t;
-	int n;
-
-	FUNC_ENTRY();
-
-	t = c->transport;
-	n = sendfile(t->socket, t->local_fd, &t->offset, t->st.st_blksize);
-	if (n > 0)
-		goto again;
-
-	send_cmd(c->sock_fd, 226, "Transfer complete.");
-	destroy_transport(c);
-again:
-	FUNC_EXIT_VOID();
-}
-
-static inline void
-upload_file(struct connection *c)
-{
-	struct transport *t;
-	char buf[65536];
-	int avail;
-	int size;
-
-	FUNC_ENTRY();
-
-	t = c->transport;
-	ioctl(t->socket, FIONREAD, &avail);
-	avail = avail > 65536 ? 65536:avail < 0 ? 0:avail;
-
-	size = recv_data(t->socket, buf, avail, 0);	
-	if (size > 0) {
-		int n = write(t->local_fd, buf, size);
-		if (n == size)
-			goto again;
-		else if (n < 0) {
-			/* ??? */
-		}
-	}
-
-	send_cmd(c->sock_fd, 226, "Transfer complete.");
-	destroy_transport(c);
-again:
-	FUNC_EXIT_VOID();
+	return 0;
 }
